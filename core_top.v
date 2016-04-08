9351453f184c4382e47699630cf2d1af3a08ee02
Alan Ehret <alan.4186@yahoo.com>
2016-04-07 23:00:50 -0400

    added ram module

--- core_top.v
+++ core_top.v
@@ -1,22 +1,21 @@
 module core_top(
-  inout [15:0] sram_dq,
+
   
   input clk, rst,
   
   output [15:0] reg_out,
-  output [18:0] sram_addr_full
+ output [18:0] sram_addr_full
 
 );
 
-wire reg_we, sram_we_n;
+wire reg_we, sram_we_n, ram_wren;
 wire [2:0] alu_operator;
 wire [3:0] reg_addr_a, reg_addr_b, reg_addr_c;
-wire [15:0] sram_addr, reg_data_a, reg_data_b, reg_data_c, alu_op_a,alu_out,alu_status, sram_q;
+wire [15:0] ram_addr, reg_data_a, reg_data_b, reg_data_c, alu_op_a,alu_out,alu_status, ram_q;
 
 assign reg_out = reg_data_a;
+assign ram_wren = ~sram_we_n;
 
-assign sram_dq = sram_we_n ? sram_q : 16'hzzzz;
-assign sram_addr_full = {3'd0, sram_addr};
 
 
 register_file register_file0(clk, rst, reg_we, reg_addr_a, reg_addr_b, reg_addr_c, reg_data_c, reg_data_a, reg_data_b);
@@ -25,8 +24,10 @@ alu16 alu16_0( clk,rst,alu_operator, alu_op_a, reg_data_b, alu_out, alu_status);
 
 control_fsm control_fsm0( clk, rst, sram_dq, reg_data_a, reg_data_b, alu_status, alu_out,
 								  sram_we_n, reg_we, alu_operator, reg_addr_a, reg_addr_b,
-								  reg_addr_c, alu_op_a, reg_data_c, sram_addr, sram_q);
+								  reg_addr_c, alu_op_a, reg_data_c, ram_addr, ram_q);
 
+
+main_memory main_memory0(ram_addr[7:0], ram_data, wren, ram_q);
 
   
 endmodule
